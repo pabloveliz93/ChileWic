86ecd13bbaa40a404003e4d1e0a75ff097a625e0
nicolasaraya77 <nicolas.arayac@mail.udp.cl>
2019-05-21 22:10:29 -0400

    login como lo dijo el diego

--- chilewic-app/app/javascript/packs/componentes/Login.jsx
+++ chilewic-app/app/javascript/packs/componentes/Login.jsx
@@ -5,6 +5,9 @@ import classNames from "classnames";
 import FlashMessage from "../FlashMessage";
 import axios from "axios";
 import * as _ from "lodash";
+
+import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+
 //import { CustomSelect } from "../CustomInputs";
 
 const LoginSchema = Yup.object().shape({
@@ -12,267 +15,61 @@ const LoginSchema = Yup.object().shape({
   country: Yup.string().required("Campo requerido")*/
 });
 
-class Login extends Component {
-  state = {
-    persona: {
-      mail: "",
-      contrasena: "",
-    },
-    ui: {
-      isSubmitting: false,
-      showMessage: false,
-      messageType: undefined,
-      message: undefined
-    }
-  };
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
 
-  componentDidMount() {
-    //Actualizar el UI de forma correcta
-    let ui = this.state.ui;
-    ui.isSubmitting = true;
-    this.setState({ ui });
-}
-    //Axios es una promesa, se debe trabajar como tal
+    this.state = {
+      email: "",
+      contrasena: ""
+    };
+  }
 
-  /*  const countries_promise = axios({
-      method: "get",
-      url: `/admin/countries.json`,
-      responseType: "json",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    });*/
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
 
-  /*  const city_promise = this.props.cityId
-      ? axios({
-          method: "get",
-          url: `/admin/cities/${this.props.cityId}/edit.json`,
-          responseType: "json",
-          headers: {
-            "Content-Type": "application/json"
-          }
-        })
-      : Promise.resolve({ data: this.state.city });*/
-
-    //Se resuelven las promesas
-    /*Promise.all([countries_promise, city_promise])
-      .then(values => {
-        const [countries_data, city_data] = values;
-        const countries = countries_data.data;
-        const city = city_data.data;
-        this.setState({ countries, city }, () => {
-          if (city.country && city.country.id) {
-            $("#country")
-              .val(city.country.id)
-              .trigger("change");
-          } else {
-            $("#country")
-              .val(countries[0].id)
-              .trigger("change");
-          }
-        });
-      })
-      .catch(error => {
-        this.showMessage({
-          show_message: true,
-          message_type: "alert-danger",
-          message: "Ha ocurrido un error inesperado"
-        });
-      })
-      .finally(() => {
-        ui.isSubmitting = false;
-        this.setState({ ui });
-      });*/
-/*
-    $("#country").select2();
-    $("#country").on("change", e => {
-      this.setState(state => {
-        let city = { ...this.state.city };
-        city.country.id = e.target.value;
-        city.country.description =
-          e.target.options[e.target.selectedIndex].text;
-        return { city };
-      });
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
     });
   }
 
-  changeCountry = country => {
-    this.state.city.country = country;
-    this.setState({ city: this.state.city });
-  };
-*/
-  showMessage = message_data => {
-    this.setState(state => {
-      state.ui.showMessage = message_data.show_message;
-      state.ui.messageType = message_data.message_type;
-      state.ui.message = message_data.message;
-      return { ui: state.ui };
-    });
-  };
+  handleSubmit = event => {
+    event.preventDefault();
+  }
 
   render() {
-    const flash_message = this.state.ui.showMessage ? (
-      <FlashMessage
-        type={this.state.ui.messageType}
-        msg={this.state.ui.message}
-      />
-    ) : null;
-    const button_label = "Agregar";//this.props.cityId ? "Editar" : "Agregar nueva";
-  /*  const countries_options = this.state.countries.map(country => (
-      <option value={country.id} key={country.id}>
-        {country.description}
-      </option>
-    ));*/
     return (
-      <div className="container">
-        {flash_message}
-        <Formik
-          showMessage={this.showMessage}
-          enableReinitialize={true}
-          initialValues={""}
-          validationSchema={LoginSchema}
-          onSubmit={(values, { setSubmitting }) => {
-            // const token = document.getElementsByName("csrf-token")[0].content;
-            const token = null;
-            /*const url = this.props.cityId
-              ? `/admin/cities/${this.props.cityId}.json`
-              : `/admin/cities.json`;*/
-            /*const method = this.props.cityId ? "PUT" : "POST";*/
-            const url = `/login.json`;
-            const method = "POST";
-            axios(url,{
-              method,
-              url,
-              responseType: "json",
-              headers: {
-                "Content-Type": "application/json"
-              },
-              data: { persona: values }
-            })
-              .then(response => {
-                if (response.data.success) {
-                  window.location.assign(`/personas?success=1`);
-                } else {
-                  this.showMessage({
-                    show_message: true,
-                    message_type: "alert-warning",
-                    message: response.data.message || "Ha ocurrido un error inesperado"
-                  });
-                }
-              })
-              .catch(error =>
-                this.showMessage({
-                  show_message: true,
-                  message_type: "alert-danger",
-                  message: "Ha ocurrido un error inesperado"
-                })
-              )
-              .then(() => setSubmitting(false));
-          }}
-        >
-          {({
-            values,
-            errors,
-            touched,
-            dirty,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting
-          }) => {
-            return (
-              <React.Fragment>
-                <form onSubmit={handleSubmit} className="">
-                  <div className="card">
-                    <div className="card-body">
-                      <div className="row">
-                        <div className="col-lg-6">
-                          <div className="form-group mb-3">
-
-
-                            <label htmlFor="mail">
-                              E-Mail
-                            </label>
-                          <Field
-                          type="text"
-                          name="mail"
-                          className={
-                            "form-control " +
-                            classNames({
-                              "is-invalid":
-                                touched.mail && errors.mail,
-                              "is-valid":
-                                touched.mail && !errors.mail
-                            })
-                          }
-                          />
-                          <ErrorMessage name="mail">
-                          {msg => (
-                            <div
-                              className="invalid-feedback"
-                              style={{ display: "block" }}
-                            >
-                              {msg}
-                            </div>
-                          )}
-                          </ErrorMessage>
-
-
-                        </div>
-                        </div>
-                        <div className="col-lg-6">
-                          <div className="form-group mb-3">
-
-
-                          <label htmlFor="contrasena">
-                          contraseña
-                          </label>
-                        <Field
-                        type="text"
-                        name="contrasena"
-                        className={
-                          "form-control " +
-                          classNames({
-                            "is-invalid":
-                              touched.apellido && errors.contrasena,
-                            "is-valid":
-                              touched.apellido && !errors.contrasena
-                          })
-                        }
-                        />
-                        <ErrorMessage name="contrasena">
-                        {msg => (
-                          <div
-                            className="invalid-feedback"
-                            style={{ display: "block" }}
-                          >
-                            {msg}
-                          </div>
-                        )}
-                        </ErrorMessage>
-
-
-
-                    </div>
-                    </div>
-                      </div>
-                      <button
-                        type="submit"
-                        className="btn btn-primary"
-                        disabled={isSubmitting}
-                      >
-                        {`${button_label} `}
-                      </button>
-                    </div>
-                  </div>
-                </form>
-              </React.Fragment>
-            );
-          }}
-        </Formik>
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            <FormLabel>Email</FormLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <FormLabel>Password</FormLabel>
+            <FormControl
+              value={this.state.contrasena}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </FormGroup>
+          <Button
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+          >
+            Login
+          </Button>
+        </form>
       </div>
     );
   }
 }
-
-export default Login;
