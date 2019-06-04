709ce2941f33a190f5119806e6fc83d9298742ca
nicolasaraya77 <nicolas.arayac@mail.udp.cl>
2019-06-03 22:06:47 -0400

    seeds iniciales de prueba

--- chilewic-app/app/controllers/api/asistencias_controller.rb
+++ chilewic-app/app/controllers/api/asistencias_controller.rb
@@ -10,15 +10,30 @@ module API
       @asistencium = Asistencium.new
     end
 
+    def show
+    render json: { status: 'SUCCESS', message: "asistencia #{@aasistencium.id} Loaded", data: @aasistencium }, status: :ok
+  end
+
+
     def create
       @asistencium = Asistencium.new(asistencium_params)
+      respond_to  do |format|
       if @asistencium.save
-        format.html { redirect_to @asistencium, notice: 'Asistencium was successfully created.' }
-        format.json { render :show, status: :created, location: @asistencium }
+
+
+
+            format.html { redirect_to  @asistencium, notice: 'Asistencium was successfully created.' }
+            format.json { render :show, status: :created, location: @asistencium }
+
+
+
+
       else
         format.html { render :new }
         format.json { render json: @asistencium.errors, status: :unprocessable_entity }
       end
+    end
+
 
 
 
